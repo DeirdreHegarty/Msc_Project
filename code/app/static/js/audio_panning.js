8f7e0a50773aff83d9ea2aebd364de7dc7b63087
Deirdre <deirdre.hegarty@mu.ie>
2018-11-06 22:11:00 +0000

    audio panning functionality - for last detected object

--- code/app/static/js/audio_panning.js
+++ code/app/static/js/audio_panning.js
@@ -6,17 +6,21 @@ function trigger_audio() {
 	const pannerOptions = {pan: 0};
 	const panner = new StereoPannerNode(audioCtx, pannerOptions);
 
-	const pannerControl = document.querySelector('#panner');
-	pannerControl.addEventListener('input', function() {
-	    panner.pan.value = this.value;
-	}, false);
-
 	bufferLoader = new BufferLoader(
 		audioCtx,
-		$.map($('audio source'), function(x) { return x.src; }),
+		$.map($('audio source'), function(x) { 
+
+			// extract the class from x [value to pan audio]
+			var read_in = x.outerHTML;
+			var str1 = read_in.substring(read_in.indexOf('class')+7);
+			var pan_value = str1.substring(0, str1.indexOf('"'));
+			panner.pan.value = pan_value;
+
+			//return the src [audio to be triggered]
+			return x.src; }),
+
 		function(bufferList) {
 			for(i=0; i<bufferList.length; i++){
-
 				var sound_effect = audioCtx.createBufferSource();
 				sound_effect.buffer = bufferList[i];
 				sound_effect.connect(panner).connect(audioCtx.destination);
