683596109754d31e81a8f2c6d1f8e68bb2753ce4
Deirdre <deirdre.hegarty@mu.ie>
2018-11-13 22:17:21 +0000

    play multiple sounds relative to position in image

--- code/app/static/js/audio_panning.js
+++ code/app/static/js/audio_panning.js
@@ -3,8 +3,8 @@ function trigger_audio() {
 	const AudioContext = window.AudioContext || window.webkitAudioContext;
 	const audioCtx = new AudioContext();
 
-	const pannerOptions = {pan: 0};
-	const panner = new StereoPannerNode(audioCtx, pannerOptions);
+	var pan_vals = [];
+	var panner = null;
 
 	bufferLoader = new BufferLoader(
 		audioCtx,
@@ -13,16 +13,28 @@ function trigger_audio() {
 			// extract the class from x [value to pan audio]
 			var read_in = x.outerHTML;
 			var str1 = read_in.substring(read_in.indexOf('class')+7);
-			var pan_value = str1.substring(0, str1.indexOf('"'));
-			panner.pan.value = pan_value;
+			this.pan_value = str1.substring(0, str1.indexOf('"'));
+			pan_vals.push(this.pan_value); 		//append position to a list
 
 			//return the src [audio to be triggered]
 			return x.src; }),
 
 		function(bufferList) {
 			for(i=0; i<bufferList.length; i++){
+
+				panner = audioCtx.createPanner();
+				panner.panningModel = 'HRTF';
+				panner.distanceModel = 'inverse';
+				panner.refDistance = 1;
+				panner.maxDistance = 10000;
+				panner.rolloffFactor = 1;
+				panner.coneInnerAngle = 360;
+				panner.coneOuterAngle = 0;
+				panner.coneOuterGain = 0;
+
 				var sound_effect = audioCtx.createBufferSource();
 				sound_effect.buffer = bufferList[i];
+				panner.positionX.value = pan_vals[i];
 				sound_effect.connect(panner).connect(audioCtx.destination);
 				sound_effect.start(i*2); // 2 second delay between sounds
 			}
