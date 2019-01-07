cd46e3d95781214133e9fb186e5cb53cfd145dc4
Deirdre <deirdre.hegarty@mu.ie>
2019-01-07 18:25:47 +0000

    Selenium - UI tests

--- code/app/tests/selenium_test.py
+++ code/app/tests/selenium_test.py
@@ -1,4 +1,4 @@
-import unittest, os
+import unittest, os, requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,7 +10,7 @@ class PythonOrgSearch(unittest.TestCase):
         self.driver = webdriver.Chrome()
         self.driver.get("http://localhost:5000/")
 
-    # testing navbar links
+    # navbar link tests
     def test_nav_thesis_link(self):
         ths = self.driver.find_element_by_id("thesis")
         ths.send_keys(Keys.RETURN)
@@ -26,18 +26,79 @@ class PythonOrgSearch(unittest.TestCase):
         mui.send_keys(Keys.RETURN)
         self.assertIn("computer-science", self.driver.current_url)
 
-    # testing accepted inputs to dropzone
+    def test_nav_brand_link(self):
+        mui = self.driver.find_element_by_class_name("navbar-brand")
+        mui.send_keys(Keys.RETURN)
+        self.assertIn("localhost", self.driver.current_url)
+
+    # upload page
+    # dropzone number of files accepted tests
     def test_accepting_one_image(self):
         self.driver.find_element_by_id("myDropzone").click()
         self.driver.find_element_by_css_selector('input[type=file]').send_keys("/Users/deirdre/Desktop/kite.jpeg")
         element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "resultrow")))
+        assert "results" in self.driver.current_url
 
     def test_accepting_multiple_images(self):
         self.driver.find_element_by_id("myDropzone").click()
         self.driver.find_element_by_css_selector('input[type=file]').send_keys("/Users/deirdre/Desktop/kite.jpeg\n/Users/deirdre/Desktop/cat.jpg")
         element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "resultrow")))
+        assert "results" not in self.driver.current_url
 
-    # testing results page
+    # image type tests
+    def test_accepting_jpeg(self):
+        self.driver.find_element_by_id("myDropzone").click()
+        self.driver.find_element_by_css_selector('input[type=file]').send_keys("/Users/deirdre/Desktop/kite.jpeg")
+        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "resultrow")))
+        assert "results" in self.driver.current_url   
+
+    def test_accepting_jpeg_u(self):
+        self.driver.find_element_by_id("myDropzone").click()
+        self.driver.find_element_by_css_selector('input[type=file]').send_keys("/Users/deirdre/Desktop/kite2.JPEG")
+        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "resultrow")))
+        assert "results" in self.driver.current_url 
+
+    def test_accepting_jpg(self):
+        self.driver.find_element_by_id("myDropzone").click()
+        self.driver.find_element_by_css_selector('input[type=file]').send_keys("Users/deirdre/Desktop/cat.jpg")
+        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "resultrow")))
+        assert "results" in self.driver.current_url  
+
+    def test_accepting_jpg_u(self):
+        self.driver.find_element_by_id("myDropzone").click()
+        self.driver.find_element_by_css_selector('input[type=file]').send_keys("Users/deirdre/Desktop/cat2.JPG")
+        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "resultrow")))
+        assert "results" in self.driver.current_url 
+
+    def test_accepting_png(self):
+        self.driver.find_element_by_id("myDropzone").click()
+        self.driver.find_element_by_css_selector('input[type=file]').send_keys("Users/deirdre/Desktop/cat.png")
+        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "resultrow")))
+        assert "results" in self.driver.current_url  
+
+    def test_accepting_png_u(self):
+        self.driver.find_element_by_id("myDropzone").click()
+        self.driver.find_element_by_css_selector('input[type=file]').send_keys("Users/deirdre/Desktop/cat.PNG")
+        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "resultrow")))
+        assert "results" in self.driver.current_url 
+
+    # div link tests
+    def test_div_thesis_link(self):
+        ths = self.driver.find_element_by_id("thesis2")
+        ths.send_keys(Keys.RETURN)
+        self.assertIn("thesis", self.driver.current_url)
+
+    def test_div_github_link(self):
+        srcode = self.driver.find_element_by_id("sourcecode2")
+        srcode.send_keys(Keys.RETURN)
+        self.assertIn("github", self.driver.current_url)
+
+    def test_div_mu_link(self):
+        mui = self.driver.find_element_by_id("mu2")
+        mui.send_keys(Keys.RETURN)
+        self.assertIn("computer-science", self.driver.current_url)   
+
+    # results page
     def test_result_text(self):
         self.driver.find_element_by_id("myDropzone").click()
         self.driver.find_element_by_css_selector('input[type=file]').send_keys("/Users/deirdre/Desktop/kite.jpeg")
@@ -54,7 +115,8 @@ class PythonOrgSearch(unittest.TestCase):
         child = parent.find_elements_by_tag_name("li")
         self.assertIn("dog",child[0].text)
         self.assertIn("cat",child[1].text)
-        
+
+    # audio tag tests   
     def test_result_amount_audio_tags(self):
         self.driver.find_element_by_id("myDropzone").click()
         self.driver.find_element_by_css_selector('input[type=file]').send_keys("/Users/deirdre/Desktop/dog-cat.jpg")
@@ -62,6 +124,31 @@ class PythonOrgSearch(unittest.TestCase):
         li_tags = self.driver.find_element_by_id("reslist").find_elements_by_tag_name("li")
         audio_tags = self.driver.find_elements_by_tag_name("audio")
         self.assertEqual(len(li_tags),len(audio_tags))
+
+    def test_result_pan_value_audio_tags(self):
+        self.driver.find_element_by_id("myDropzone").click()
+        self.driver.find_element_by_css_selector('input[type=file]').send_keys("/Users/deirdre/Desktop/dog-cat.jpg")
+        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "resultrow")))
+        audio_tags = self.driver.find_elements_by_tag_name("audio")
+        source_tags = []
+        for x in audio_tags:
+            xs = 0.0
+            try:
+                xs = float(x.find_element_by_tag_name("source").get_attribute("class"))
+            except ValueError:
+                xs = False
+            self.assertTrue(isinstance( xs, float))
+    
+    def test_result_src_audio_tags(self):
+        self.driver.find_element_by_id("myDropzone").click()
+        self.driver.find_element_by_css_selector('input[type=file]').send_keys("/Users/deirdre/Desktop/dog-cat.jpg")
+        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "resultrow")))
+        audio_tags = self.driver.find_elements_by_tag_name("audio")
+        source_tags = []
+        for x in audio_tags:
+            r = requests.head(x.find_element_by_tag_name("source").get_attribute("src"))
+            self.assertEqual( 200, r.status_code )
+  
 
     def tearDown(self):
         self.driver.close()
